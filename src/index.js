e0d2fb8c2c84bc0eeb81b7f2ffdb65e30d4d2560
yurubahmed <yurubiftin2002@gmail.com>
2022-12-12 00:22:44 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector('form');
+
+  form.addEventListener("submit", (event)=>{
+     event.preventDefault();
+     let input = document.getElementById('new-task-description').value;
+      append(input);
+      form.reset();
+  })
+   
+})
+
+
+const append = (input) => {
+  let displayer = document.getElementById("tasks");
+  let li = document.createElement("li");
+  li.className += " " + "mt-3";
+  li.textContent = input
+  let row = displayer.appendChild(li)
+
+
+  let btn = document.createElement("button");
+  btn.className += " " + "btn btn-xm btn-danger ml-5";
+  btn.textContent = "X";
+  row.appendChild(btn)
+
+  btn.onclick = () => (delete_(li)) ;
+  
+}
+
+const delete_ = (li) => {
+   li.remove();
+} 
+
